82861b159d26e94aefb8bd293ea8c89461ce94dd
William Novak <william@send4store.com>
2018-01-11 09:59:46 -0200

    fix

--- Shipping/Model/Carrier/Send4Shipping.php
+++ Shipping/Model/Carrier/Send4Shipping.php
@@ -188,7 +188,7 @@ class Send4Shipping extends \Magento\Shipping\Model\Carrier\AbstractCarrier impl
         $address = $dot['address'] . " - " . $dot['complement'] . " - ".$dot['neighbor'] . ", " . $dot['zip_code'];
 
         $method = $this->_rateMethodFactory->create();
-        $method->setCarrier($this->_code . "_{$i}");
+        $method->setCarrier($this->_code);
         $method->setCarrierTitle($this->getConfigData('title'));
         $method->setMethod($this->_code . "_{$i}");
         $method->setMethodTitle($dot['id'] . " - " . $dot['trade_name'] . " - " . $dot['complement']);
